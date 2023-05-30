3f38a2ca84329eaccc9920882ecf9c3aee0251a1
Mustakim <sycospike1@gmail.com>
2023-05-31 00:11:42 +0600

    added delete

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qroq892.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,11 +40,27 @@ async function run() {
         res.send(result);
     })
 
-    //carts collection
+    //carts collection apis
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email ;
+      if(!email){
+        res.send([]);
+      }
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
     app.post('/carts', async(req, res) => {
       const item = req.body ;
       console.log(item);
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.delete('/carts/:id', async(req, res) => {
+      const id = req.params.id ;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
